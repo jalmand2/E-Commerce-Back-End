c242995f1ff4fe3570b980ef7739a1ddf18c9cc6
Jessica Almand <jlalmand27@gmail.com>
2022-11-06 22:01:55 -0500

    Added post route to tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,7 +32,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
-
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
