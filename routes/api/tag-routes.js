682d938ae39885577fdf55c8a3a1254986708fd0
Jessica Almand <jlalmand27@gmail.com>
2022-11-07 11:26:24 -0500

    Just pushing up changes to work off of later

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,6 +44,16 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTagById = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({updateTagById});
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
